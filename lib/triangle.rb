06413afcd90832f1403078b23169a4605109b286
John Carter <johncarter352@gmail.com>
2020-04-27 05:12:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,10 @@
 class Triangle
- attr_accessor :arg1, :arg2, :arg3 
+ attr_accessor :s1, :s2, :s3 
  
- def initialize (arg1, arg2, arg3)
-   
+ def initialize (s1, s2, s3)
+   @s1 = s1
+   @s2 = s2
+   @s3 = s3
 end
 
 class TraingleError < StandardError
