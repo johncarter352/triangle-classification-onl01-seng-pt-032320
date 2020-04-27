1b2cf58979f730e704eaebc158815c609ab115c0
John Carter <johncarter352@gmail.com>
2020-04-27 05:17:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,23 @@ class Triangle
 end
 
   def type
-    if 
+    if (@s1 <= 0) || (@s2 <= 0) || (@s3 <= 0)
+      raise TriangleError
+    elsif (@s1+@s2 <= @si3) || (@side_1+@s3 <= @s2) || (@s2+@s3 <= @side_1)
+      raise TriangleError
+    else
+      if (@s1 == @s2) && (@s2 == @s3)
+        :equilateral
+      elsif (@s1 == @s2) || (@s2 == @s3) || (@s1 == @s3)
+        :isosceles
+      elsif (@s1 != @s2) && (@s2 != @s3) && (@s1 != @s3)
+        :scalene
+      end
+    end
 
 
 end 
 
 class TraingleError < StandardError
-  #code 
+
 end
